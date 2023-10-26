3853d82e4a9ada06905064ea646fe9acbbf1cfe2
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-10-26 17:28:33 +0300

    omni > TWRP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X655D-user
-add_lunch_combo omni_Infinix-X655D-userdebug
-add_lunch_combo omni_Infinix-X655D-eng
+add_lunch_combo twrp_Infinix-X655D-user
+add_lunch_combo twrp_Infinix-X655D-userdebug
+add_lunch_combo twrp_Infinix-X655D-eng
