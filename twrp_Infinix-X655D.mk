01a6b2b54e920513196e443f49b4fa7e2dccb305
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-10-26 17:27:43 +0300

    GSI keys

--- twrp_Infinix-X655D.mk
+++ twrp_Infinix-X655D.mk
@@ -9,21 +9,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from Infinix-X655D device
 $(call inherit-product, device/infinix/Infinix-X655D/device.mk)
 
 PRODUCT_DEVICE := Infinix-X655D
-PRODUCT_NAME := omni_Infinix-X655D
+PRODUCT_NAME := twrp_Infinix-X655D
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X655D
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_x655d_h6211-user 10 QP1A.190711.020 126584 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X655D-IN/Infinix-X655D:10/QP1A.190711.020/M-AS-210706V273:user/release-keys
